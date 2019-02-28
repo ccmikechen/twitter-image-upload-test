4fb7ea277fba87b7420aefd291fdba07d40823f3
ccmikechen <ccmikechen@gmail.com>
2019-02-28 18:40:06 +0800

    Add image

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import Twitter from 'twitter'
  
 const client = new Twitter({
-  consumer_key: 'QjhM882cOrkAgTdRdd85p4gD3',
-  consumer_secret: 'gzxoJCVXsys7ZnA0jaSkWonOgnkIEwMIZfZuQUR0ezZeU2poHy',
-  access_token_key: '1032570257645297664-ZB4yQhN7OsOkOxcXNSX8jkwrqirqxh',
-  access_token_secret: 'BglaAnaizsMFONGUaVa3XI3xamCPLl6l5w3wrUgiILpS4'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 })
 
-const params = { screen_name: 'nodejs' }
-client.get('statuses/user_timeline', params, (error, tweets, response) => {
+const data = require('fs').readFileSync('BTC-JPY.png');
+
+client.post('media/upload', { media: data }, (error, media, response) => {
   if (!error) {
-    console.log(tweets)
+    console.log(media);
+
+    var status = {
+      status: '',
+      media_ids: media.media_id_string
+    }
+
+    client.post('statuses/update', status, (error, tweet, response) => {
+      if (!error) {
+        console.log(tweet)
+      }
+    })
+
   }
 })
